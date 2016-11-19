11cc633afd3a43e9c9294b8c5d54bad0f40b0d7b
manishSRM <manishchaurasiya101@gmail.com>
2016-11-19 22:00:21 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,15 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g
+from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "my key"
+app.database = "sample.db"
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You need to login first")
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/')
+@login_required
 def home():
-    return "Hello World!!!"
-
+    # return "Hello World!!"
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title = row[0], description = row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template("template.html", posts=posts)
 
 @app.route('/welcome')
 def welcome():
     return render_template("welcome.html")
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != "admin":
+            error = "u r not an admin!!"
+            return render_template('login.html', error=error)
+        else:
+            session['logged_in'] = True
+            flash("You are just logged in!")
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash("you are just logged out!")
+    return redirect(url_for('welcome'))
+
+
+def connect_db():
+    return sqlite3.connect(app.database)
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
